e62093ec1e0a075382dfe7f878ef5b636c6602e1
Matt Burnside <mattburnside@gmail.com>
2020-07-24 10:31:40 -0500

    Another prod build with relative path

---
+++ docs/precache-manifest.d36fa8581f481fe816b03f2937e5b08d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23bbf6de3143513000879d43d37bcc3a",
+    "url": "/www/index.html"
+  },
+  {
+    "revision": "0c6119e3eaf39bcf634b",
+    "url": "/www/static/css/main.4f12ba25.chunk.css"
+  },
+  {
+    "revision": "b4295c7d3d778d7bb234",
+    "url": "/www/static/js/2.f395e05e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/www/static/js/2.f395e05e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6119e3eaf39bcf634b",
+    "url": "/www/static/js/main.0d7afa6e.chunk.js"
+  },
+  {
+    "revision": "a46e27897fa12f56b21c",
+    "url": "/www/static/js/runtime-main.184f04d4.js"
+  },
+  {
+    "revision": "c488b1244c66683a542eebe437e08f84",
+    "url": "/www/static/media/AbsinthePoster_Cropped.c488b124.jpg"
+  },
+  {
+    "revision": "13ba2e010620b709c67a2f10ca97118a",
+    "url": "/www/static/media/CopperplateGothicBold.13ba2e01.eot"
+  },
+  {
+    "revision": "1765d330ab0960593da4e978b437c740",
+    "url": "/www/static/media/CopperplateGothicBold.1765d330.svg"
+  },
+  {
+    "revision": "aa0fe4ee31013499c5947077d45f4656",
+    "url": "/www/static/media/CopperplateGothicBold.aa0fe4ee.woff"
+  },
+  {
+    "revision": "b22afb9d7f41ae60d566a48f36cd4fde",
+    "url": "/www/static/media/CopperplateGothicBold.b22afb9d.ttf"
+  },
+  {
+    "revision": "e860d8845d67d722d2250c9d4f6b04f8",
+    "url": "/www/static/media/CopperplateGothicBold.e860d884.woff2"
+  },
+  {
+    "revision": "0bb88f6f4ca7f2ee7872a05a760e3a32",
+    "url": "/www/static/media/CopperplateGothicHeavyBT.0bb88f6f.eot"
+  },
+  {
+    "revision": "566aa35e067386f87ea7b6af8de35cfa",
+    "url": "/www/static/media/CopperplateGothicHeavyBT.566aa35e.woff"
+  },
+  {
+    "revision": "8070c5ff7795a512a1f6efd4f08ec907",
+    "url": "/www/static/media/CopperplateGothicHeavyBT.8070c5ff.svg"
+  },
+  {
+    "revision": "87b6195a5337667c4279239e8bf416c5",
+    "url": "/www/static/media/CopperplateGothicHeavyBT.87b6195a.ttf"
+  },
+  {
+    "revision": "f1aca07a5afbe85f4ca25d761e2a8d23",
+    "url": "/www/static/media/CopperplateGothicHeavyBT.f1aca07a.woff2"
+  },
+  {
+    "revision": "977492b391944d3f1f8c8c12848c1c16",
+    "url": "/www/static/media/WineBackgroundImage.977492b3.jpg"
+  },
+  {
+    "revision": "7e967035466e0951f2dbddd0440d3c58",
+    "url": "/www/static/media/iPhoneWithLogoTrans.7e967035.png"
+  }
+]);
